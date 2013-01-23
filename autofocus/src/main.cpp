b47cb76e75d957e7ffbcf1efeb1a9742dc832390
Yegor Alexeyev <yegor.alexeyev@gmail.com>
2013-01-23 12:00:08 +0400

    Separated yuy2 namespace into the separate header file

--- autofocus/src/main.cpp
+++ autofocus/src/main.cpp
@@ -45,49 +45,8 @@
 #include "BufferReference.h"
 
 #include "FocusAnalyser.h"
+#include "yuy2.h"
 
-
-
-namespace yuy2 {
-	struct luma_t {};
-
-// chroma-blue and chroma-red fields are not distinguished
-	struct chroma_t {};
-
-	typedef boost::mpl::vector2<luma_t,chroma_t> color_space_t;
-
-	typedef boost::gil::layout<color_space_t> pixel_layout_t;
-
-//    template <typename, typename>    struct pixel;
-//    template <typename, typename>    struct planar_pixel_reference;
-//    template <typename, typename>    struct planar_pixel_iterator;
-//    template <typename>                class memory_based_step_iterator;
-//    template <typename>                class point2;
-//    template <typename>                class memory_based_2d_locator;
-//    template <typename>                class image_view;
-//    template <typename, bool, typename>    class image;
-    typedef boost::gil::pixel<boost::gil::bits8, pixel_layout_t> pixel_t;
-    typedef const boost::gil::pixel<boost::gil::bits8, pixel_layout_t> c_pixel_t;
-    typedef boost::gil::pixel<boost::gil::bits8, pixel_layout_t>& ref_t;
-    typedef const boost::gil::pixel<boost::gil::bits8, pixel_layout_t>& c_ref_t;
-    typedef pixel_t* ptr_t;
-    typedef c_pixel_t* c_ptr_t;
-    typedef boost::gil::memory_based_step_iterator<ptr_t>                               step_ptr_t;
-    typedef boost::gil::memory_based_step_iterator<c_ptr_t>                               c_step_ptr_t;
-    typedef boost::gil::memory_based_2d_locator<boost::gil::memory_based_step_iterator<ptr_t> >       loc_t;
-    typedef boost::gil::memory_based_2d_locator<boost::gil::memory_based_step_iterator<c_ptr_t> >       c_loc_t;
-    typedef boost::gil::memory_based_2d_locator<boost::gil::memory_based_step_iterator<step_ptr_t> >  step_loc_t;
-    typedef boost::gil::memory_based_2d_locator<boost::gil::memory_based_step_iterator<c_step_ptr_t> > c_step_loc_t;
-    typedef boost::gil::image_view<loc_t>                                        view_t;
-    typedef boost::gil::image_view<c_loc_t>                                        c_view_t;
-    typedef boost::gil::image_view<step_loc_t>                                    step_view_t;
-    typedef boost::gil::image_view<c_step_loc_t>                                   c_step_view_t;
-    typedef boost::gil::image<pixel_t,false,std::allocator<unsigned char> >           image_t;
-
-
-//	typedef boost::gil::image_type<boost::gil::bits8,pixel_layout_t> image_t;
-//    GIL_DEFINE_BASE_TYPEDEFS
-};
 
 
 void fillWithLumaFromYUY2(uint8_t yuy2[],size_t width,size_t height,uint8_t luma[]) {
