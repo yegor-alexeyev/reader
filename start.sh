786a1d39ca7a7583cc4c230765442a32cb6c30ea
Yegor Alexeyev <yegor.alexeyev@gmail.com>
2013-02-03 19:53:40 +0400

    Updated launch script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
-rm /dev/mqueue/video0-*
-camera/Release/camera > camera.log&
-LD_LIBRARY_PATH=/home/yegor/opencv-release-eclipse-project/lib autofocus/Release/autofocus
-killall camera
-
-
+rm -f /dev/shm/video0-*
+(camera/Release/camera > camera.log; date)&
+LD_LIBRARY_PATH=/home/yegor/opencv-release-eclipse-project/lib autofocus/Release/autofocus&
+trap 'killall autofocus; killall camera' EXIT
+cd /dev/shm
+while true; do rm -f $(find -mmin +0.09 -and -name 'video0-ready-*'); sleep 0.002; done
